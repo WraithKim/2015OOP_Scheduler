838c496a08ef8f0596ad57339f06b090760a1b60
nErumin <nErumin90@gmail.com>
2015-11-30 14:47:48 +0900

    Add util package and Constant class.

---
+++ src/util/Constant.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * 프로젝트 진행 시 공통적으로 필요한 상수들을 정의해 놓은 클래스입니다. (정적 클래스)
+ */
+public class Constant {
+    public static final String EMPTY_STRING = "";
+
+    /**
+     * Constant 클래스의 인스턴스를 초기화합니다. Constant 클래스는 정적 클래스이므로 외부에서 인스턴스화 할 수 없습니다.
+     */
+    private Constant() {
+
+    }
+}
