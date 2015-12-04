f41de79998c4dc2c06d52d5a15c3944cefa1dd23
Donghwan Kim <wraithtorch@gmail.com>
2015-12-05 02:12:48 +0900

    init Scheduler

--- src/main/Scheduler.java
+++ src/main/Scheduler.java
@@ -5,11 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import schedule.AlarmThread;
+import util.AlarmQueue;
 import util.Constant;
 import util.FileManager;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Donghwan on 11/29/2015.
@@ -25,35 +30,74 @@ public class Scheduler extends Application{
     public Scheduler() throws FileNotFoundException{
     }
 
-    public void loadAlarmQueue(){
+    public void initAlarmQueue(){
         // 현재 시간에서 8일 후까지의 일정을 불러온다.
         // 알람큐에 등록
+        Calendar cur = GregorianCalendar.getInstance();
+        AlarmQueue alarmQueue = AlarmQueue.getInstance();
+        for(int i = 0; i < 8; i++){
+            try{
+                alarmQueue.addAll(FileManager.getInstance().readFile(cur.get(Calendar.YEAR), cur.get(Calendar.MONTH), cur.get(Calendar.DAY_OF_MONTH)));
+            }catch(IOException ioe) {
+
+            }catch(ClassNotFoundException cnfe){
+                System.err.println("Something wrong in Data directory");
+                System.exit(1);
+                return;
+            }
+        }
+
+        while(alarmQueue.peek().getAlarmTime() < cur.getTimeInMillis()){
+            alarmQueue.poll();
+        }
+
+        AlarmThread.getInstance().start();
     }
 
-    public void loadStudentId() throws ClassNotFoundException, IOException{
+    private void initStudentId() throws ClassNotFoundException, IOException{
         // 학생의 학번을 불러옴
         try {
             Constant.savedStudentID = FileManager.getInstance().readStudentNumber();
         }catch(FileNotFoundException fnfe){
             // 설정 창을 열어서 id를 입력받게 해야함.
+            Parent root = FXMLLoader.load(getClass().getResource(Constant.viewPackage+Constant.SettingView));
+            Scene scene = new Scene(root);
+            Stage settingView = new Stage();
+
+            settingView.setTitle("New Student ID");
+            settingView.setScene(scene);
+            settingView.setAlwaysOnTop(true);
+            settingView.setResizable(false);
+            settingView.setAlwaysOnTop(true);
+            settingView.show();
+            settingView.setOnCloseRequest((WindowEvent event)->{
+                try{
+                    FileManager.getInstance().readStudentNumber();
+                }catch(Exception e){
+                    System.exit(1);
+                }
+                settingView.close();
+            });
         }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         // 스케쥴러 관리 프로그램을 생성하고
-        loadStudentId();
-        loadAlarmQueue();
-
+        initStudentId();
+        initAlarmQueue();
 
         // 달력 뷰를 생성
-        Parent root = FXMLLoader.load(getClass().getResource(Constant.viewPackage+Constant.SettingView));
+        Parent root = FXMLLoader.load(getClass().getResource(Constant.viewPackage+Constant.CalendarView));
         Scene scene = new Scene(root);
 
         primaryStage.setTitle("Calendar");
         primaryStage.setScene(scene);
-        primaryStage.setAlwaysOnTop(true);
         primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest((WindowEvent event)->{
+            // 끄기 전에 저장을 할거면 여기에 저장하는 코드를 넣어야 함
+            System.exit(0);
+        });
 
         primaryStage.show();
     }
