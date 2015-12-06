d4d4a2c9c62cd6a1937679c5e26d72d845411199
nErumin <nErumin90@gmail.com>
2015-12-06 23:57:37 +0900

    Refactor Homework Class again.

--- src/util/Homework.java
+++ src/util/Homework.java
@@ -1,9 +1,11 @@
 package util;
 
+import javafx.beans.property.SimpleStringProperty;
 import schedule.Priority;
 import schedule.Schedule;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
@@ -14,8 +16,20 @@ import java.util.Calendar;
 
 public class Homework extends Schedule implements Serializable {
     private static final long serialVersionUID = 20151206L;
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd");
+    private transient SimpleStringProperty dateProperty;
 
     public Homework(String name, Calendar dueDate) {
         super(name, dueDate, Priority.NOTICED);
+        dateProperty = new SimpleStringProperty();
+        dateProperty.set(dateFormat.format(getDueDate().getTime()));
+    }
+
+    public SimpleStringProperty datePropertyProperty() {
+        if(dateProperty == null){
+            dateProperty = new SimpleStringProperty();
+            dateProperty.set(dateFormat.format(getDueDate().getTime()));
+        }
+        return dateProperty;
     }
 }
